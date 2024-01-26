b487d418b354760e84c1cbe3bafae99108a81ef8
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-01-26 10:11:52 -0500

    Finish template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import './style.css';
+import "./style.css";
 
-const text = document.createElement('p');
-const div = document.querySelector('.container');
-
-text.textContent = 'Testing';
-text.classList.add('hello');
-div.appendChild(text);
 
